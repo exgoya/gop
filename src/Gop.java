f0dccceab419e95c5713b16cd7d4628c5093cc08
shotgoal <shotgoal@gmail.com>
2022-08-31 12:28:56 +0900

    modified path added goldilocks8.jar

--- src/Gop.java
+++ src/Gop.java
@@ -4,23 +4,49 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Properties;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-
+import sunje.goldilocks.jdbc.GoldilocksDriver;
 
 public class Gop {
+	public static Properties properties = new Properties();
+	public static Connection con_goldi = null;
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
+		//db information from properties
+		FileReader resources = null;
+		resources = new FileReader("resource/db.properties");
+		properties.load(resources);
+		String URL_NODE1 = properties.getProperty("goldi_url");
+		Properties sProp = new Properties();
+		sProp.put("user", properties.getProperty("goldi_user"));
+		sProp.put("password", properties.getProperty("goldi_password"));
+		try {
+			Class.forName("sunje.goldilocks.jdbc.GoldilocksDriver");
+			con_goldi = DriverManager.getConnection(URL_NODE1, sProp);
+			con_goldi.setAutoCommit(false);
+			DatabaseMetaData md = con_goldi.getMetaData();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.toString());
+		}
+		
 		//read config
-		try(FileInputStream fis = new FileInputStream("config.json");
+		try(FileInputStream fis = new FileInputStream("resource/config.json");
 		    InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
 		    BufferedReader reader = new BufferedReader(isr))
 		{
