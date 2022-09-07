d8e4ab4ee8e062c294254aadc367321962180afd
exgoya <exgoya@gmail.com>
2022-09-07 17:00:21 +0900

    update shell

--- resource/run.sh
+++ resource/run.sh
@@ -1,1 +1,7 @@
-java -cp ./lib/gson-2.9.1.jar:./bin Gop
+javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar src/model/*.java -d bin/
+javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar:bin/ src/service/*.java -d bin/
+javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar:bin/ src/*.java -d bin/
+
+cd bin
+jar -cvmf ../MANIFEST.MF gop.jar *.class model/*.class service/*.class
+mv gop.jar ../.
