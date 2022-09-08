3d4f31c850541dd32e174bb07617f96fb039510b
exgoya <bam125@naver.com>
2022-09-08 13:21:42 +0900

    Update comp.sh

--- resource/comp.sh
+++ resource/comp.sh
@@ -1,3 +1,5 @@
+mkdir bin
+
 javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar src/model/*.java -d bin/
 javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar:bin/ src/service/*.java -d bin/
 javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar:bin/ src/*.java -d bin/
