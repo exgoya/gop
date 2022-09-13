1678ef0cedca78ef1aedbeeeeea0dd6fad114b72
exgoya <bam125@naver.com>
2022-09-13 14:26:31 +0900

    add return in manifest

--- resource/comp.sh
+++ resource/comp.sh
@@ -6,7 +6,6 @@ javac -cp lib/gson-2.9.1.jar:lib/goldilocks8.jar:bin/ src/*.java -d bin/
 
 
 cd bin
-mv ../MANIFEST.MF ../MANIFEST.MF 
 
 jar -cvmf ../MANIFEST.MF gop.jar *.class model/*.class service/*.class
 mv gop.jar ../.
