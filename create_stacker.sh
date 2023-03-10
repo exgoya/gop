6a7476746e9106e4f8a63b3b2956f3ba45f25455
exgoya <bam125@naver.com>
2023-03-10 15:31:03 +0900

    Update create_stacker.sh

--- create_stacker.sh
+++ create_stacker.sh
@@ -6,3 +6,5 @@ curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=
 curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=peer'
 curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=lock'
 curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=long_Tx'
+curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=buffer_hit'
+curl -v -X POST 'http://192.168.0.120:5108/apis/create-measure?db-name=gop&name=buffer_miss'
